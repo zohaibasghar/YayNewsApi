12673dfbe84c428b6dc891765ed267e43fab6d57
Mian Zohaib Asghar <zohaibmehar123456@gmail.com>
2023-04-06 20:07:44 +0500

    ''

--- src/components/News.js
+++ src/components/News.js
@@ -56,43 +56,45 @@ export default function News(props) {
   };
 
   return (
-    <div className="container">
-      <h2 className="text-center my-3">Yay News - Top headlines</h2>
-      <h3 className="text-center">
-        {category[0].toUpperCase() + category.slice(1)} News
-      </h3>
-      {loading && <Loading />}
-      <InfiniteScroll
-        className="scroll"
-        dataLength={article.articles.length}
-        next={fetchMoreData}
-        hasMore={article.articles.length !== article.totalResults}
-        loader={<Loading />}
-      >
-        <div className="">
-          <div className="row">
-            {article.articles.map((element, index) => {
-              return (
-                <div className="col-md mx-1" key={index}>
-                  <NewsItem
-                    title={element.title}
-                    newSrc={element.source.name}
-                    author={element.author}
-                    description={element.description}
-                    publish={element.publishedAt}
-                    source={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://s.yimg.com/ny/api/res/1.2/yxm.F4F1J3pDIqSzHgGglQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD05MDA-/https://media.zenfs.com/en/business_insider_articles_888/a926720d1f53948beddbd852c36c9f66"
-                    }
-                    read={element.url}
-                  />
-                </div>
-              );
-            })}
+    article.articles && (
+      <div className="container">
+        <h2 className="text-center my-3">Yay News - Top headlines</h2>
+        <h3 className="text-center">
+          {category[0].toUpperCase() + category.slice(1)} News
+        </h3>
+        {loading && <Loading />}
+        <InfiniteScroll
+          className="scroll"
+          dataLength={article.articles.length}
+          next={fetchMoreData}
+          hasMore={article.articles.length !== article.totalResults}
+          loader={<Loading />}
+        >
+          <div className="">
+            <div className="row">
+              {article.articles.map((element, index) => {
+                return (
+                  <div className="col-md mx-1" key={index}>
+                    <NewsItem
+                      title={element.title}
+                      newSrc={element.source.name}
+                      author={element.author}
+                      description={element.description}
+                      publish={element.publishedAt}
+                      source={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://s.yimg.com/ny/api/res/1.2/yxm.F4F1J3pDIqSzHgGglQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD05MDA-/https://media.zenfs.com/en/business_insider_articles_888/a926720d1f53948beddbd852c36c9f66"
+                      }
+                      read={element.url}
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </InfiniteScroll>
-    </div>
+        </InfiniteScroll>
+      </div>
+    )
   );
 }
